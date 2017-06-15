b8cf9d666c94401a2c0ad6c500b80c036146dc7a
SilcoDev <silco2017@outlook.com>
2017-06-15 18:28:46 +0200

    SilCo

--- src/main.h
+++ src/main.h
@@ -47,10 +47,10 @@ static const int64_t MIN_TX_FEE = 1 * COIN;
 /** Fees smaller than this (in satoshi) are considered zero fee (for relaying) */
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 /** No amount larger than this (in satoshi) is valid */
-static const int64_t MAX_MONEY = 500000000000000 * COIN; // 50 Billion
+static const int64_t MAX_MONEY = 5000000000000000 * COIN; // 50 Billion
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 /** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
-static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
+static const unsigned int LOCKTIME_THRESHOLD = 5000000000000000; // Tue Nov  5 00:53:20 1985 UTC
 static const int64_t COIN_YEAR_REWARD = 125 * CENT; // 125% per year
 
 inline int64_t FutureDrift(int64_t nTime) { return nTime + 10 * 60; }
